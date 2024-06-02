c76a0f4fde505cce618d7e07c749223779eff153
AugustoCVS <guty.vertolis@gmail.com>
2024-06-02 20:48:01 -0300

    feat: create user provider

---
+++ src/providers/user.provider.tsx
@@ -0,0 +1,42 @@
+import { setUser } from "@/redux/slices/User/user.slice";
+import { UserProps } from "@/services/interfaces/user";
+import { UserService } from "@/services/user";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useQuery } from "@tanstack/react-query";
+import { jwtDecode } from "jwt-decode";
+import { ReactNode } from "react";
+import { useDispatch } from "react-redux";
+import { useMessage } from "src/utils/message";
+
+const ERROR_MESSAGE = "Erro ao buscar dados do uusário";
+
+export const UserProvider = ({ children }: { children: ReactNode }) => {
+  const { showToast } = useMessage();
+  const dispatch = useDispatch();
+
+  const handleGetUser = async (): Promise<void> => {
+    const token = await AsyncStorage.getItem("token");
+    const userId = jwtDecode<UserProps>(token!).id;
+
+    await UserService.getUser({ userId })
+      .then((res) => {
+        dispatch(setUser(res));
+      })
+      .catch(() => {
+        showToast({
+          title: ERROR_MESSAGE,
+          error: true,
+        });
+      });
+  };
+
+  useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      await handleGetUser();
+      return null;
+    },
+  });
+
+  return <>{children}</>;
+};
