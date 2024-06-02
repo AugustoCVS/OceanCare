6e84948c7a51b920b619b4f6304445927794cc92
AugustoCVS <guty.vertolis@gmail.com>
2024-06-02 20:00:05 -0300

    feat: add token store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/User/user.slice";
+import tokenReducer from "./slices/Token/token.slice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    token: tokenReducer,
   },
 });
 
