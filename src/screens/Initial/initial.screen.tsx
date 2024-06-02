1acc812653499c1e072183a725f16c84edc8324b
AugustoCVS <guty.vertolis@gmail.com>
2024-06-02 17:08:40 -0300

    feat: add register modal on initial screen

--- src/screens/Initial/initial.screen.tsx
+++ src/screens/Initial/initial.screen.tsx
@@ -2,23 +2,31 @@ import { View, Text } from "react-native";
 
 import { Button } from "@/components/Button/button.component";
 import Logo from "src/assets/logo-initial.svg";
+import { useInitial } from "./initial.hook";
+import { ModalRegister } from "./components/ModalRegister/modal-register.component";
 
 export const Initial: React.FC = () => {
+  const { actions, refs } = useInitial();
+
   return (
-    <View className="bg-blue-100 flex flex-col w-full h-full items-center justify-between p-20">
-      <View className="pt-20">
-        <Logo />
+    <>
+      <View className="bg-blue-100 flex flex-col w-full h-full items-center justify-between p-20">
+        <View className="pt-20">
+          <Logo />
+        </View>
+
+        <View className="flex flex-col gap-4">
+          <Button isFirstStyle>
+            <Text className="text-black text-lg font-bold">Login</Text>
+          </Button>
+
+          <Button onPress={actions.handleOpenModalRegister}>
+            <Text className="text-black text-lg font-bold">Registrar</Text>
+          </Button>
+        </View>
       </View>
 
-      <View className="flex flex-col gap-4">
-        <Button isFirstStyle>
-          <Text className="text-black text-lg font-bold">Login</Text>
-        </Button>
-
-        <Button>
-          <Text className="text-black text-lg font-bold">Registrar</Text>
-        </Button>
-      </View>
-    </View>
+      <ModalRegister modalRef={refs.modalRegisterRef} />
+    </>
   );
 };
