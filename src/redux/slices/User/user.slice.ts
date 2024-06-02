94c562f1b19a1a01fed27d3408c08aa04bb69f60
AugustoCVS <guty.vertolis@gmail.com>
2024-06-02 19:13:07 -0300

    feat: create user slice

---
+++ src/redux/slices/User/user.slice.ts
@@ -0,0 +1,28 @@
+import { UserProps } from 'src/services/interfaces/user';
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+const initialState: UserProps = {
+  id: "",
+  name: "",
+  email: "",
+  reportedTrash: 0,
+  achievements: [],
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<UserProps>) => {
+      state.id = action.payload.id;
+      state.name = action.payload.name;
+      state.email = action.payload.email;
+      state.reportedTrash = action.payload.reportedTrash;
+      state.achievements = action.payload.achievements;
+    },
+  },
+});
+
+export const { setUser } = userSlice.actions;
+
+export default userSlice.reducer;
