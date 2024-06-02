9e30b61dc9c010a313315cf94a0631bd43021710
AugustoCVS <guty.vertolis@gmail.com>
2024-06-02 16:34:55 -0300

    feat: add primary style on button

--- src/components/Button/button.component.tsx
+++ src/components/Button/button.component.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {TouchableOpacity, TouchableOpacityProps} from 'react-native';
+import React from "react";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
 
 type ButtonProps = TouchableOpacityProps & {
   children?: React.ReactNode;
@@ -12,7 +12,14 @@ export const Button: React.FC<ButtonProps> = ({
   className,
   ...rest
 }) => (
-  <TouchableOpacity {...rest} className={className}>
+  <TouchableOpacity
+    {...rest}
+    className={
+      className
+        ? className
+        : "bg-blue-200 w-72 h-16 rounded-lg flex items-center justify-center text-center"
+    }
+  >
     {children}
   </TouchableOpacity>
 );
