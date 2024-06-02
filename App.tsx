a6e6fba92dd91ffbd3ad7e3e204d6a79daf405be
AugustoCVS <guty.vertolis@gmail.com>
2024-06-02 19:18:47 -0300

    feat: add redux provider

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Provider } from "react-redux";
 import "react-native-gesture-handler";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
@@ -7,6 +8,7 @@ import { Host } from "react-native-portalize";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import StackComponent from "./src/routes/stack.routes";
+import { store } from "@/redux/store";
 
 const queryClient = new QueryClient();
 
@@ -15,10 +17,12 @@ function App(): JSX.Element {
     <GestureHandlerRootView style={{ flex: 1 }}>
       <NativeBaseProvider>
         <Host>
-          <QueryClientProvider client={queryClient}>
-            <StatusBar style="dark" />
-            <StackComponent />
-          </QueryClientProvider>
+          <Provider store={store}>
+            <QueryClientProvider client={queryClient}>
+              <StatusBar style="dark" />
+              <StackComponent />
+            </QueryClientProvider>
+          </Provider>
         </Host>
       </NativeBaseProvider>
     </GestureHandlerRootView>
