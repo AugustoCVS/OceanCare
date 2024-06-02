92bc1f2eea46f1b0b4f264f097fa1f04aa09ccc6
AugustoCVS <guty.vertolis@gmail.com>
2024-06-02 17:34:48 -0300

    feat: add query client provider on app

--- App.tsx
+++ App.tsx
@@ -4,16 +4,21 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { NativeBaseProvider } from "native-base";
 import { Host } from "react-native-portalize";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import StackComponent from "./src/routes/stack.routes";
+
+const queryClient = new QueryClient();
 
 function App(): JSX.Element {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <NativeBaseProvider>
         <Host>
-          <StatusBar style="dark" />
-          <StackComponent />
+          <QueryClientProvider client={queryClient}>
+            <StatusBar style="dark" />
+            <StackComponent />
+          </QueryClientProvider>
         </Host>
       </NativeBaseProvider>
     </GestureHandlerRootView>
