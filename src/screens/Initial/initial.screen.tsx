fb3d5ae4d3a0f58c90f3961835aa85d55cccb497
AugustoCVS <guty.vertolis@gmail.com>
2024-06-02 16:52:49 -0300

    feat: create initial screen

--- src/screens/Initial/initial.screen.tsx
+++ src/screens/Initial/initial.screen.tsx
@@ -1,9 +1,24 @@
 import { View, Text } from "react-native";
 
+import { Button } from "@/components/Button/button.component";
+import Logo from "src/assets/logo-initial.svg";
+
 export const Initial: React.FC = () => {
   return (
-    <View className="w-full h-full items-center justify-center">
-      <Text>Initial</Text>
+    <View className="bg-blue-100 flex flex-col w-full h-full items-center justify-between p-20">
+      <View className="pt-20">
+        <Logo />
+      </View>
+
+      <View className="flex flex-col gap-4">
+        <Button isFirstStyle>
+          <Text className="text-black text-lg font-bold">Login</Text>
+        </Button>
+
+        <Button>
+          <Text className="text-black text-lg font-bold">Registrar</Text>
+        </Button>
+      </View>
     </View>
   );
 };
