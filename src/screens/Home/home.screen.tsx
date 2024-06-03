a306808a4b6cc85285a3007d0a87c0d596101917
AugustoCVS <guty.vertolis@gmail.com>
2024-06-03 15:47:52 -0300

    feat: add modal report trash

--- src/screens/Home/home.screen.tsx
+++ src/screens/Home/home.screen.tsx
@@ -1,10 +1,16 @@
 import { View, Text } from "react-native";
 import { Header } from "./components/Header/header.component";
+import { ModalReportTrash } from "@/components/Modais/ModalReportTrash/modal-report-trash.component";
+import { useHome } from "./home.hook";
 
 export const Home: React.FC = () => {
+  const { refs, actions } = useHome();
+
   return (
     <>
-      <Header handleOpenModal={() => {}} />
+      <Header handleOpenModal={actions.handleOpenModalReportTrash} />
+
+      <ModalReportTrash modalRef={refs.modalReportTrashRef} />
     </>
   );
 };
