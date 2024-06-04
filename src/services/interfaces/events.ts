b30487cc4bc10130dfb55e5ab64819bfbc6930d5
AugustoCVS <guty.vertolis@gmail.com>
2024-06-04 01:22:47 -0300

    feat: create events service

---
+++ src/services/interfaces/events.ts
@@ -0,0 +1,33 @@
+import { UserProps } from "./user";
+
+export interface MetaData {
+  pageNumber: number;
+  pageSize: number;
+}
+
+export type EventsProps = {
+  id: number;
+  name: string;
+  description: string;
+  location: string;
+  startDate: string;
+  endDate: string;
+  users: UserProps[];
+};
+
+export type GetEventsByUserProps = {
+  userId: number;
+  page: number;
+  size: number;
+};
+
+export type GetEventsResponse = {
+  pageable: MetaData;
+  content: EventsProps[];
+}
+
+export type SubscribeUserOnEventProps = {
+  eventId: number;
+  userId: number;
+};
+
