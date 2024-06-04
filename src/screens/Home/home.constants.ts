95769d7a35859300ffacbc1c93905bd0d19232bd
AugustoCVS <guty.vertolis@gmail.com>
2024-06-03 23:44:23 -0300

    feat: create CTA list for home

---
+++ src/screens/Home/home.constants.ts
@@ -0,0 +1,16 @@
+export const CTAList = [
+  {
+    id: 0,
+    icon: 'cleaning',
+    title: 'Reportar lixo',
+    description: 'Ajude a manter o oceano limpo',
+    action: 'reportTrash',
+  },
+  {
+    id: 1,
+    icon: 'ranking',
+    title: 'Ranking',
+    description: 'Veja quem está ajudando mais',
+    action: 'ranking',
+  }
+]
