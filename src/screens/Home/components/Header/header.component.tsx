c85d8b2be017724e3530a5da63158930ab829515
AugustoCVS <guty.vertolis@gmail.com>
2024-06-03 21:21:09 -0300

    feat: add temporary logout

--- src/screens/Home/components/Header/header.component.tsx
+++ src/screens/Home/components/Header/header.component.tsx
@@ -7,12 +7,12 @@ import { Button } from "@/components/Button/button.component";
 import { HeaderProps } from "./header.types";
 
 export const Header: React.FC<HeaderProps> = ({ handleOpenModal }) => {
-  const { states } = useHeader();
+  const { states, actions } = useHeader();
 
   return (
     <View className="w-full h-40 bg-blue-100 flex flex-row items-center justify-between px-4 pt-8">
-      <Logo width={80} height={80} />
-      <Title text={states.user.name} />
+      <Logo width={80} height={80} onPress={actions.handleLogout} />
+      <Title text={states.user?.name} />
       <Button
         text="Reportar"
         className="bg-blue-300 w-24 h-12 rounded-lg flex items-center justify-center text-center"
