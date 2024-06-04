7c84b36a14184ae39e8473a3fe34573100a2c918
AugustoCVS <guty.vertolis@gmail.com>
2024-06-03 23:44:32 -0300

    feat: add CTAs

--- src/screens/Home/home.screen.tsx
+++ src/screens/Home/home.screen.tsx
@@ -1,11 +1,26 @@
+import { Octicons, FontAwesome6 } from "@expo/vector-icons";
+import { ScrollView } from "react-native-gesture-handler";
+
 import { Header } from "./components/Header/header.component";
 import { ModalReportTrash } from "@/components/Modais/ModalReportTrash/modal-report-trash.component";
 import { useHome } from "./home.hook";
-import { ScrollView } from "react-native-gesture-handler";
 import { Achievements } from "./components/Achievements/achievements.component";
+import { CTA } from "@/components/CTA/cta.component";
+import { CTAList } from "./home.constants";
+import { View } from "react-native";
 
 export const Home: React.FC = () => {
   const { refs, actions } = useHome();
+
+  const renderCtaIcons: Record<string, JSX.Element> = {
+    cleaning: <Octicons name="report" size={28} color="#173042FC" />,
+    ranking: <FontAwesome6 name="ranking-star" size={28} color="#173042FC" />,
+  };
+
+  const renderActions: Record<string, () => void> = {
+    reportTrash: actions.handleOpenModalReportTrash,
+    ranking: () => {},
+  };
 
   return (
     <>
@@ -22,6 +37,18 @@ export const Home: React.FC = () => {
         }}
       >
         <Achievements />
+
+        <View className="flex flex-col">
+          {CTAList.map((cta) => (
+            <CTA
+              key={cta.id}
+              description={cta.description}
+              icon={renderCtaIcons[cta.icon]}
+              onPress={renderActions[cta.action]}
+              title={cta.title}
+            />
+          ))}
+        </View>
       </ScrollView>
     </>
   );
