f9c6b1831be8db24985bd3b81e2a6820617f750a
AugustoCVS <guty.vertolis@gmail.com>
2024-06-04 01:23:02 -0300

    feat: create get events by user

--- src/screens/Home/home.hook.ts
+++ src/screens/Home/home.hook.ts
@@ -1,16 +1,39 @@
+import { RootState } from "@/redux/store";
+import { EventsService } from "@/services/events";
+import { EventsProps } from "@/services/interfaces/events";
+import { useQuery } from "@tanstack/react-query";
 import { useRef } from "react";
 import { Modalize } from "react-native-modalize";
+import { useSelector } from "react-redux";
 
 export const useHome = () => {
+  const user = useSelector((state: RootState) => state.user);
+
   const modalReportTrashRef = useRef<Modalize>(null);
 
   const handleOpenModalReportTrash = (): void => {
     modalReportTrashRef.current?.open();
   };
 
+  const { data, isLoading } = useQuery({
+    queryKey: ["events"],
+    queryFn: async () => await EventsService.getEventsByUser({
+      userId: user.id,
+      page: 0,
+      size: 20,
+     }),
+    refetchOnWindowFocus: false,
+  })
+
+  const eventsList = data?.content
+
   return {
     refs: {
       modalReportTrashRef,
+    },
+    states: {
+      isLoading,
+      eventsList,
     },
     actions: {
       handleOpenModalReportTrash,
