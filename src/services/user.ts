fd05d0a4bc2a8053ffbb58dc3f5b300b39c9654c
AugustoCVS <guty.vertolis@gmail.com>
2024-06-03 21:12:00 -0300

    refactor: change userId type

--- src/services/user.ts
+++ src/services/user.ts
@@ -2,7 +2,7 @@ import { api } from "./api";
 import { UserProps } from "./interfaces/user";
 
 export const UserService = {
-  getUser: async ({ userId }: { userId: string }) => {
+  getUser: async ({ userId }: { userId: number }) => {
     const res = await api.get<UserProps>(`/users/${userId}`);
 
     return res.data;
