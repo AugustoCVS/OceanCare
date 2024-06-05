40847aae640dafdb6c91caf50ab370a63ab83696
AugustoCVS <guty.vertolis@gmail.com>
2024-06-05 18:38:57 -0300

    feat: add setUser

--- src/screens/Menu/components/ModalUpdateInfo/modal-update-info.hook.ts
+++ src/screens/Menu/components/ModalUpdateInfo/modal-update-info.hook.ts
@@ -4,12 +4,15 @@ import { useMessage } from "src/utils/message";
 import { UserService } from "@/services/user";
 import { UpdateUserInfoRequest } from "@/services/interfaces/user";
 import { ERROR_MESSAGE, SUCCESS_MESSAGE } from "./modal-update-info.utils";
+import { useDispatch } from "react-redux";
+import { setUser } from "@/redux/slices/User/user.slice";
 
 export const useModalUpdateInfo = ({
   modalRef,
   userInfo,
 }: ModalUpdateInfoProps) => {
   const { showToast } = useMessage();
+  const dispatch = useDispatch();
 
   const handleCloseModal = (): void => {
     modalRef.current?.close();
@@ -19,7 +22,8 @@ export const useModalUpdateInfo = ({
     mutationKey: ["updateUserInfo"],
     mutationFn: async (data: UpdateUserInfoRequest) =>
       await UserService.updateUserInfo({ data, userId: userInfo.id }),
-    onSuccess: () => {
+    onSuccess: (res) => {
+      dispatch(setUser(res));
       showToast({
         title: SUCCESS_MESSAGE,
       });
