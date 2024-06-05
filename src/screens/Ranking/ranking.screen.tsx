90698c1d1df735c933f3b88f5c74c91625f2e35d
AugustoCVS <guty.vertolis@gmail.com>
2024-06-05 16:55:22 -0300

    feat: add UserList

--- src/screens/Ranking/ranking.screen.tsx
+++ src/screens/Ranking/ranking.screen.tsx
@@ -1,10 +1,11 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import { Header } from "./components/Header/header.component";
 import { useRanking } from "./ranking.hook";
 import { ModalReportTrash } from "@/components/Modais/ModalReportTrash/modal-report-trash.component";
+import { UserList } from "./components/UserList/user-list.component";
 
 export const Ranking: React.FC = () => {
-  const { refs, actions } = useRanking();
+  const { refs, states, actions } = useRanking();
 
   return (
     <>
@@ -12,6 +13,12 @@ export const Ranking: React.FC = () => {
         <Header
           handleOpenModal={actions.handleOpenModalReportTrash}
           navigateToHome={actions.handleNavigateToHome}
+        />
+
+        <UserList
+          userList={states.userList}
+          isLoading={states.isLoading}
+          onRefresh={actions.handleRefetch}
         />
       </View>
 
