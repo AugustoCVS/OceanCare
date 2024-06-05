69273715a0b0ca46c427a3102279569300671999
AugustoCVS <guty.vertolis@gmail.com>
2024-06-05 16:53:33 -0300

    feat: get users list from api

--- src/screens/Ranking/ranking.hook.ts
+++ src/screens/Ranking/ranking.hook.ts
@@ -1,28 +1,55 @@
 import { TabTypes } from "@/routes/tab.routes";
+import { UserService } from "@/services/user";
+import { useMessage } from "@/utils/message";
 import { useNavigation } from "@react-navigation/native";
+import { useQuery } from "@tanstack/react-query";
 import { useRef } from "react";
 import { Modalize } from "react-native-modalize";
+import { ERROR_MESSAGE } from "./ranking.constants";
 
 export const useRanking = () => {
   const navigation = useNavigation<TabTypes>();
+  const { showToast } = useMessage();
 
   const modalReportTrashRef = useRef<Modalize>(null);
 
   const handleOpenModalReportTrash = (): void => {
     modalReportTrashRef.current?.open();
-  }
+  };
 
   const handleNavigateToHome = (): void => {
     navigation.navigate("Home");
+  };
+
+  const { data, isLoading, refetch } = useQuery({
+    queryKey: ["ranking"],
+    queryFn: async () =>
+      await UserService.getUsers().catch(() => {
+        showToast({
+          title: ERROR_MESSAGE,
+          error: true,
+        });
+      }),
+  });
+
+  const userList = data?.content || [];
+
+  const handleRefetch = (): void => {
+    refetch();
   }
 
   return {
     refs: {
       modalReportTrashRef,
     },
+    states: {
+      isLoading,
+      userList,
+    },
     actions: {
       handleOpenModalReportTrash,
       handleNavigateToHome,
-    }
-  }
-}
+      handleRefetch,
+    },
+  };
+};
