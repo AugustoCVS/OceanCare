7fe49264bf693dec51aff7a461393368fc5f6ed2
AugustoCVS <guty.vertolis@gmail.com>
2024-06-05 18:21:50 -0300

    feat: add menu screen

--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -1,11 +1,17 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { AntDesign, MaterialIcons, FontAwesome6 } from "@expo/vector-icons";
+import {
+  AntDesign,
+  MaterialIcons,
+  FontAwesome6,
+  Ionicons,
+} from "@expo/vector-icons";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import { Home } from "../screens/Home/home.screen";
 import { Events } from "@/screens/Events/events.screen";
 import { Ranking } from "@/screens/Ranking/ranking.screen";
+import { Menu } from "@/screens/Menu/menu.screen";
 
 const Tab = createBottomTabNavigator();
 
@@ -13,6 +19,7 @@ export type TabNavigation = {
   Home: undefined;
   Events: undefined;
   Ranking: undefined;
+  Menu: undefined;
 };
 
 export type TabTypes = NativeStackNavigationProp<TabNavigation>;
@@ -68,6 +75,17 @@ export default function TabRoutes() {
           tabBarLabel: "Ranking",
         }}
         component={Ranking}
+      />
+
+      <Tab.Screen
+        name="Menu"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="menu-outline" size={20} color={color} />
+          ),
+          tabBarLabel: "Menu",
+        }}
+        component={Menu}
       />
     </Tab.Navigator>
   );
